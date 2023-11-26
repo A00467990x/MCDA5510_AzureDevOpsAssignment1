fb624a44f6fa091ae1a84e6b659ecd8c9283035c
A00467990x <144293496+A00467990x@users.noreply.github.com>
2023-11-26 13:20:15 -0400

    first commit on DEV

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
 
       <p>
-          This is PROD
+          This is DEV
         </p>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
